dd485def8e292666572d37a68afd74522bf124f3
conrad mugabe <conradmugabe@gmail.com>
2023-03-18 01:01:24 +0300

    feat: added game platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,10 +2,17 @@ import { CanceledError } from 'axios';
 import { useEffect, useState } from 'react';
 import apiClientService from '../services/api.client.service';
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
 
 interface GetGamesResponse {
