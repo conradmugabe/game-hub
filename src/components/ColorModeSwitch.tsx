372503945bedfac64edcd982e39e9f05d8f9f9da
conrad mugabe <conradmugabe@gmail.com>
2023-03-17 23:39:22 +0300

    fix: switch size

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -2,7 +2,14 @@ import { Switch, useColorMode } from '@chakra-ui/react';
 
 function ColorModeSwitch() {
   const { colorMode, toggleColorMode } = useColorMode();
-  return <Switch isChecked={colorMode === 'dark'} onChange={toggleColorMode} />;
+  return (
+    <Switch
+      isChecked={colorMode === 'dark'}
+      onChange={toggleColorMode}
+      size="lg"
+      colorScheme="blue"
+    />
+  );
 }
 
 export default ColorModeSwitch;
