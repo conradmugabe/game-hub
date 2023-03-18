334f0224eda8a393e3709a61a910e181066006ad
conrad mugabe <conradmugabe@gmail.com>
2023-03-18 11:25:05 +0300

    fix: added genre name to game heading

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 function GameHeading({ gameQuery }: Props) {
   const heading = `${gameQuery.platform?.name || ''} ${
-    gameQuery.platform?.name || ''
+    gameQuery.genre?.name || ''
   } Games`;
   return (
     <Heading as="h1" fontSize="5xl">
