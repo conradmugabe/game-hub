3dcc5c58e22257de284edbc1001e366ef61e2342
conrad mugabe <conradmugabe@gmail.com>
2023-04-21 14:19:40 +0300

    feat: made app responsive

--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -1,9 +1,8 @@
-import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import { GridItem, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
-import DefinitionItem from "../components/DefinitionItem";
+
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
-import GameCriticScore from "../components/GameCriticScore";
 import GameScreenshots from "../components/GameScreenshots";
 import GameTrailer from "../components/GameTrailer";
 
@@ -11,17 +10,21 @@ import useGame from "../hooks/useGame";
 
 export default function GameDetailsPage() {
   const { slug } = useParams();
-  const { data: game, error, isLoading } = useGame(slug!);
+  const { data: game, error } = useGame(slug!);
 
   if (game)
     return (
-      <>
-        <Heading>{game.name}</Heading>
-        <ExpandableText text={game.description_raw} />
-        <GameAttributes game={game} />
-        <GameTrailer gameId={game.id} />
-        <GameScreenshots gameId={game.id} />
-      </>
+      <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
+        <GridItem>
+          <Heading>{game.name}</Heading>
+          <ExpandableText text={game.description_raw} />
+          <GameAttributes game={game} />
+        </GridItem>
+        <GridItem>
+          <GameTrailer gameId={game.id} />
+          <GameScreenshots gameId={game.id} />
+        </GridItem>
+      </SimpleGrid>
     );
 
   if (error) throw error;
